5613710d4d3666067ba290bb2380b5f4282f92d5
dustinheestand <heestand@post.harvard.edu>
2020-12-02 21:15:04 -0800

    solve day 3

--- days/day3/day3.go
+++ days/day3/day3.go
@@ -4,11 +4,15 @@ import (
 	"bufio"
 	"log"
 	"os"
-	"sort"
 	"strconv"
 )
 
-var input sort.IntSlice
+type forest struct {
+	width int
+	trees [](map[int]bool)
+}
+
+var input forest
 
 func init() {
 	file, err := os.Open("input/day3.txt")
@@ -18,22 +22,51 @@ func init() {
 	}
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		txt := scanner.Text()
-		i, err := strconv.Atoi(txt)
-		if err != nil {
-			log.Fatal(err)
+		bytes := scanner.Bytes()
+		input.width = len(bytes)
+		trees := make(map[int]bool)
+		for i, b := range bytes {
+			if b == '#' {
+				trees[i] = true
+			}
 		}
-		input = append(input, i)
+		input.trees = append(input.trees, trees)
 	}
-	input.Sort()
 }
 
 // Solve1 solves.
 func Solve1() string {
-	return ""
+	var res, col int
+	for _, row := range input.trees {
+		if row[col%input.width] {
+			res++
+		}
+		col += 3
+	}
+	return strconv.Itoa(res)
+}
+
+type slope struct {
+	right, down int
 }
 
 // Solve2 solves.
 func Solve2() string {
-	return ""
+	slopes := []slope{{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}}
+	var encounters []int
+	for _, s := range slopes {
+		var col, hits int
+		for rowNum := 0; rowNum < len(input.trees); rowNum += s.down {
+			if input.trees[rowNum][col%input.width] {
+				hits++
+			}
+			col += s.right
+		}
+		encounters = append(encounters, hits)
+	}
+	res := 1
+	for _, e := range encounters {
+		res *= e
+	}
+	return strconv.Itoa(res)
 }
