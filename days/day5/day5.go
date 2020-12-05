0270477658004dcd96c7e89ea389dc8679b29de3
dustinheestand <heestand@post.harvard.edu>
2020-12-04 21:17:49 -0800

    solve day 5

--- days/day5/day5.go
+++ days/day5/day5.go
@@ -6,9 +6,29 @@ import (
 	"os"
 	"sort"
 	"strconv"
+	"strings"
 )
 
-var input sort.IntSlice
+type seat struct {
+	row, col int
+}
+
+type seats []seat
+
+var input seats
+
+func (ss seats) Len() int      { return len(ss) }
+func (ss seats) Swap(i, j int) { ss[i], ss[j] = ss[j], ss[i] }
+func (ss seats) Less(i, j int) bool {
+	a, b := ss[i], ss[j]
+	if a.row < b.row {
+		return true
+	}
+	if a.row == b.row && a.col < b.col {
+		return true
+	}
+	return false
+}
 
 func init() {
 	file, err := os.Open("input/day5.txt")
@@ -19,21 +39,40 @@ func init() {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		txt := scanner.Text()
-		i, err := strconv.Atoi(txt)
-		if err != nil {
-			log.Fatal(err)
-		}
-		input = append(input, i)
+		rowStr := txt[:7]
+		colStr := txt[7:]
+		rowStr = strings.Replace(rowStr, "F", "0", -1)
+		rowStr = strings.Replace(rowStr, "B", "1", -1)
+		colStr = strings.Replace(colStr, "L", "0", -1)
+		colStr = strings.Replace(colStr, "R", "1", -1)
+		row, _ := strconv.ParseInt(rowStr, 2, 32)
+		col, _ := strconv.ParseInt(colStr, 2, 32)
+		input = append(input, seat{int(row), int(col)})
 	}
-	input.Sort()
+	sort.Sort(input)
 }
 
 // Solve1 solves.
 func Solve1() string {
-	return ""
+	var max int
+	for _, s := range input {
+		idx := s.row*8 + s.col
+		if max < idx {
+			max = idx
+		}
+	}
+	return strconv.Itoa(max)
 }
 
 // Solve2 solves.
 func Solve2() string {
+	lastIdx := -100
+	for _, s := range input {
+		idx := s.row*8 + s.col
+		if idx-lastIdx == 2 {
+			return strconv.Itoa(idx - 1)
+		}
+		lastIdx = idx
+	}
 	return ""
 }
