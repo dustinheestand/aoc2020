ef645302a6b6f7d69c0756365e47cc3025c422f2
dustinheestand <heestand@post.harvard.edu>
2020-12-09 21:52:37 -0800

    solve day 10

--- days/day10/day10.go
+++ days/day10/day10.go
@@ -17,6 +17,7 @@ func init() {
 		log.Fatal(err)
 	}
 	scanner := bufio.NewScanner(file)
+	input = append(input, 0)
 	for scanner.Scan() {
 		txt := scanner.Text()
 		i, err := strconv.Atoi(txt)
@@ -30,10 +31,35 @@ func init() {
 
 // Solve1 solves.
 func Solve1() string {
-	return ""
+	oneDiffs, threeDiffs := 0, 1
+	last := 0
+	for _, n := range input[1:] {
+		if n-last == 1 {
+			oneDiffs++
+		} else if n-last == 3 {
+			threeDiffs++
+		}
+		last = n
+	}
+	return strconv.Itoa(oneDiffs * threeDiffs)
 }
 
 // Solve2 solves.
 func Solve2() string {
-	return ""
+	ways := []int{0, 0, 1}
+	for i, n := range input {
+		if i == 0 {
+			continue
+		}
+		waysToN := 0
+		for j := 1; j <= 3 && i-j >= 0; j++ {
+			if n-input[i-j] > 3 {
+				break
+			}
+			waysToN += ways[3-j]
+		}
+		ways = append(ways, waysToN)
+		ways = ways[1:]
+	}
+	return strconv.Itoa(ways[2])
 }
