ea11414f0bc0b0288ce5a17a3edf1c3bf6d17b3b
dustinheestand <heestand@post.harvard.edu>
2020-12-14 21:38:52 -0800

    solve day 15

--- days/day15/day15.go
+++ days/day15/day15.go
@@ -5,11 +5,11 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"sort"
 	"strconv"
+	"strings"
 )
 
-var input sort.IntSlice
+var input []int
 
 func init() {
 	file, err := os.Open("input/day15.txt")
@@ -20,21 +20,47 @@ func init() {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		txt := scanner.Text()
-		i, err := strconv.Atoi(txt)
-		if err != nil {
-			log.Fatal(err)
+		fs := strings.FieldsFunc(txt, func(r rune) bool { return r == ',' })
+		for _, f := range fs {
+			i, err := strconv.Atoi(f)
+			if err != nil {
+				log.Fatal(err)
+			}
+			input = append(input, i)
 		}
-		input = append(input, i)
 	}
-	input.Sort()
 }
 
 // Solve1 solves.
 func Solve1() string {
-	return fmt.Sprint("")
+	return solve(2020)
 }
 
 // Solve2 solves.
 func Solve2() string {
-	return fmt.Sprint("")
+	return solve(30000000)
+}
+
+func solve(pos int) string {
+	seen := make(map[int]int)
+	next := -1
+	for i := 0; ; i++ {
+		if i < len(input) {
+			seen[input[i]] = i + 1
+			continue
+		}
+		if i == pos {
+			return fmt.Sprint(next)
+		}
+		last, ok := seen[next]
+		since := i - last
+		if i != len(input) {
+			seen[next] = i
+		}
+		if !ok {
+			next = 0
+		} else {
+			next = since
+		}
+	}
 }
