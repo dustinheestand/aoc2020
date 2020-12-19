58efaaa29a666424df02898857da94ddf6541f38
Dustin Heestand <cheerfully@gmail.com>
2020-12-18 20:48:53 -0800

    scaffold

---
+++ days/day24/day24.go
@@ -0,0 +1,40 @@
+package day24
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var input sort.IntSlice
+
+func init() {
+	file, err := os.Open("input/day24.txt")
+	defer file.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		txt := scanner.Text()
+		i, err := strconv.Atoi(txt)
+		if err != nil {
+			log.Fatal(err)
+		}
+		input = append(input, i)
+	}
+	input.Sort()
+}
+
+// Solve1 solves.
+func Solve1() string {
+	return fmt.Sprint("")
+}
+
+// Solve2 solves.
+func Solve2() string {
+	return fmt.Sprint("")
+}
