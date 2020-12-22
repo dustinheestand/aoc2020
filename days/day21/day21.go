a0d6412fc60e93e43249e993aa1de7e4c114bd35
Dustin Heestand <cheerfully@gmail.com>
2020-12-21 20:32:35 -0800

    clean up

--- days/day21/day21.go
+++ days/day21/day21.go
@@ -10,9 +10,10 @@ import (
 )
 
 var (
-	input          []recipe
-	allAllergens   map[string](map[string]bool)
-	maybeAllergens map[string]bool
+	input                   []recipe
+	ingCount                map[string]int
+	allergensToMatchingIngs map[string](map[string]bool)
+	maybeAllergenicIngs     map[string]bool
 )
 
 type recipe struct {
@@ -26,8 +27,9 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	allAllergens = make(map[string](map[string]bool))
-	maybeAllergens = make(map[string]bool)
+	ingCount = make(map[string]int)
+	allergensToMatchingIngs = make(map[string](map[string]bool))
+	maybeAllergenicIngs = make(map[string]bool)
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		txt := scanner.Text()
@@ -40,6 +42,9 @@ func init() {
 				o = 1
 				continue
 			}
+			if o == 0 {
+				ingCount[f]++
+			}
 			t[f[:len(f)-o]] = true
 		}
 		input = append(input, r)
@@ -47,10 +52,10 @@ func init() {
 
 	for _, r := range input {
 		for a := range r.allergens {
-			if allergens, ok := allAllergens[a]; !ok {
-				allAllergens[a] = map[string]bool{}
+			if allergens, ok := allergensToMatchingIngs[a]; !ok {
+				allergensToMatchingIngs[a] = map[string]bool{}
 				for i := range r.ingredients {
-					allAllergens[a][i] = true
+					allergensToMatchingIngs[a][i] = true
 				}
 			} else {
 				for a := range allergens {
@@ -61,9 +66,9 @@ func init() {
 			}
 		}
 	}
-	for _, is := range allAllergens {
+	for _, is := range allergensToMatchingIngs {
 		for i := range is {
-			maybeAllergens[i] = true
+			maybeAllergenicIngs[i] = true
 		}
 	}
 }
@@ -71,11 +76,9 @@ func init() {
 // Solve1 solves.
 func Solve1() string {
 	res := 0
-	for _, r := range input {
-		for i := range r.ingredients {
-			if !maybeAllergens[i] {
-				res++
-			}
+	for ing, count := range ingCount {
+		if !maybeAllergenicIngs[ing] {
+			res += count
 		}
 	}
 	return fmt.Sprint(res)
@@ -85,7 +88,7 @@ func Solve1() string {
 func Solve2() string {
 	seen := make(map[string]string)
 	for len(seen) < 8 {
-		for a, is := range allAllergens {
+		for a, is := range allergensToMatchingIngs {
 			for i := range is {
 				if seen[i] != "" && seen[i] != a {
 					delete(is, i)
@@ -99,15 +102,15 @@ func Solve2() string {
 		}
 	}
 
-	allergens := sort.StringSlice{}
-	for a := range allAllergens {
+	var allergens sort.StringSlice
+	for a := range allergensToMatchingIngs {
 		allergens = append(allergens, a)
 	}
 	allergens.Sort()
 
-	ings := []string{}
+	var ings []string
 	for _, a := range allergens {
-		for i := range allAllergens[a] {
+		for i := range allergensToMatchingIngs[a] {
 			ings = append(ings, i)
 		}
 	}
