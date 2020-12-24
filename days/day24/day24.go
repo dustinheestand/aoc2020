8e83e9bb68b9699b4565a2a225debf6381589010
Dustin Heestand <cheerfully@gmail.com>
2020-12-23 21:29:20 -0800

    solve day 24

--- days/day24/day24.go
+++ days/day24/day24.go
@@ -5,11 +5,11 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"sort"
-	"strconv"
 )
 
-var input sort.IntSlice
+var input []tile
+
+type tile []string
 
 func init() {
 	file, err := os.Open("input/day24.txt")
@@ -20,21 +20,118 @@ func init() {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		txt := scanner.Text()
-		i, err := strconv.Atoi(txt)
-		if err != nil {
-			log.Fatal(err)
+		s := ""
+		t := tile{}
+		for _, r := range txt {
+			s += string(r)
+			if r == 'e' || r == 'w' {
+				t = append(t, s)
+				s = ""
+			}
 		}
-		input = append(input, i)
+		input = append(input, t)
 	}
-	input.Sort()
+}
+
+type coord struct {
+	rise, run int
+}
+
+func (c *coord) move(dir string) {
+	switch dir {
+	case "ne":
+		c.rise++
+	case "nw":
+		c.rise++
+		c.run--
+	case "e":
+		c.run++
+	case "w":
+		c.run--
+	case "se":
+		c.rise--
+		c.run++
+	case "sw":
+		c.rise--
+	}
 }
 
 // Solve1 solves.
 func Solve1() string {
-	return fmt.Sprint("")
+	changes := make(map[coord]bool)
+	for _, t := range input {
+		c := &coord{}
+		for _, mv := range t {
+			c.move(mv)
+		}
+		changes[*c] = !changes[*c]
+	}
+
+	res := 0
+	for _, c := range changes {
+		if c {
+			res++
+		}
+	}
+	return fmt.Sprint(res)
+}
+
+func (c *coord) adjs() []coord {
+	var res []coord
+	for _, dir := range []string{"e", "w", "ne", "nw", "se", "sw"} {
+		newC := coord{c.rise, c.run}
+		newC.move(dir)
+		res = append(res, newC)
+	}
+	return res
 }
 
 // Solve2 solves.
 func Solve2() string {
-	return fmt.Sprint("")
+	blackTiles := make(map[coord]bool)
+	for _, t := range input {
+		c := &coord{}
+		for _, mv := range t {
+			c.move(mv)
+		}
+		blackTiles[*c] = !blackTiles[*c]
+	}
+
+	for i := 0; i < 100; i++ {
+		flips := make(map[coord]bool)
+		for c, isBlack := range blackTiles {
+			if !isBlack {
+				delete(blackTiles, c)
+				continue
+			}
+			for _, t := range append(c.adjs(), c) {
+				adjBlack := 0
+				for _, adjT := range t.adjs() {
+					if blackTiles[adjT] {
+						adjBlack++
+					}
+				}
+				if blackTiles[t] {
+					if adjBlack < 1 || adjBlack > 2 {
+						flips[t] = true
+					}
+				} else {
+					if adjBlack == 2 {
+						flips[t] = true
+					}
+				}
+			}
+		}
+		for f := range flips {
+			blackTiles[f] = !blackTiles[f]
+		}
+	}
+
+	res := 0
+	for _, c := range blackTiles {
+		if c {
+			res++
+		}
+	}
+	return fmt.Sprint(res)
 }
--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module github.com/dustinheestand/aoc2020
 
 go 1.15
-
-require (
-	github.com/google/pprof v0.0.0-20201218002935-b9804c9f04c2 // indirect
-	golang.org/x/sys v0.0.0-20201221093633-bc327ba9c2f0 // indirect
-)
